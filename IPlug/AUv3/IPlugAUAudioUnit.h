d79e052b7d1e25c1fac053b63bbdc16e786e5f3f
Oli Larkin <olilarkin@googlemail.com>
2022-09-03 14:08:39 +0200

    WIP: AUv3: Add support for User Presets

--- IPlug/AUv3/IPlugAUAudioUnit.h
+++ IPlug/AUv3/IPlugAUAudioUnit.h
@@ -34,4 +34,5 @@
 - (PLATFORM_VIEW*) openWindow: (PLATFORM_VIEW*) pParent;
 - (void) closeWindow;
 - (bool) sendMidiData:(int64_t) sampleTime : (NSInteger) length : (const uint8_t*) midiBytes;
+- (NSArray<AUAudioUnitPreset*>*) getUserPresets;
 @end
