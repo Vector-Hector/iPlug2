fb4212da538d58c67a5dbae63d3498f097bfd893
Oli Larkin <olilarkin@googlemail.com>
2024-01-28 15:36:46 +0100

    WIP: default arg

--- IGraphics/Controls/IWebViewControl.h
+++ IGraphics/Controls/IWebViewControl.h
@@ -42,7 +42,7 @@ public:
    * @param readyFunc A function conforming to onReadyFunc, that will be called asyncronously when the webview has been initialized
    * @param msgFunc A function conforming to onMessageFunc, that will be called when messages are posted from the webview
    * @param enableDevTools Should the webview developer tools be available via context menu */
-  IWebViewControl(const IRECT& bounds, bool opaque, OnReadyFunc readyFunc, OnMessageFunc msgFunc = nullptr, bool enableDevTools = false)
+  IWebViewControl(const IRECT& bounds, bool opaque, OnReadyFunc readyFunc = nullptr, OnMessageFunc msgFunc = nullptr, bool enableDevTools = false)
   : IControl(bounds)
   , IWebView(opaque)
   , mOnReadyFunc(readyFunc)
